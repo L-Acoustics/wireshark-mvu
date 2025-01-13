e2a82cc10d890ab4243a90c1607007c6a55e58f5
benjamin.landrot <benjamin.landrot@l-acoustics.com>
2025-01-13 14:08:45 +0100

    Add system unique ID field for SET_SYSTEM_UNIQUE_ID commands and GET_SYSTEM_UNIQUE_ID reponses

--- mvu.lua
+++ mvu.lua
@@ -57,6 +57,7 @@ local f_mvu_protocol_version = ProtoField.uint32("mvu.protocol_version", "Protoc
 local f_mvu_feature_flags = ProtoField.uint32("mvu.feature_flags", "Feature Flages", base.HEX)
 local f_mvu_feature_redundancy = ProtoField.bool("mvu.features.redundancy", "REDUNDANCY", 32, nil, 0x00000001)
 local f_mvu_feature_talker_dynamic_mappings_while_running = ProtoField.bool("mvu.features.talker_dynamic_mappings", "TALKER_DYNAMIC_MAPPINGS_WHILE_RUNNING", 32, nil, 0x00000002)
+local f_mvu_system_unique_id = ProtoField.uint32("mvu.system_unique_id", "System Unique ID", base.HEX)
 
 -- Add fields to protocol
 milan_proto.fields = {
@@ -65,6 +66,7 @@ milan_proto.fields = {
     f_mvu_feature_flags,
     f_mvu_feature_redundancy,
     f_mvu_feature_talker_dynamic_mappings_while_running,
+    f_mvu_system_unique_id,
 }
 
 --- Implementation of protocol's dissector
@@ -128,12 +130,12 @@ function milan_proto.dissector(buffer, pinfo, tree)
                     mvuSubtree:add(f_mvu_command_type, buffer(mvu_payload_start, 2), command_type):append_text(" (" .. command_type_description .. ")")
 
                     ---
-                    --- Responses to GET_MILAN_INFO or GET_SYSTEM_UNIQUE_ID
+                    --- Responses to GET_MILAN_INFO
                     --- 
                     
-                    -- If the message is a reponse to GET_MILAN_INFO or GET_SYSTEM_UNIQUE_ID
+                    -- If the message is a reponse to GET_MILAN_INFO
                     if message_type == IEEE17221.AECP_MESSAGE_TYPES.VENDOR_UNIQUE_RESPONSE
-                    and (command_type == MVU.COMMAND_TYPES.GET_MILAN_INFO or command_type == MVU.COMMAND_TYPES.GET_SYSTEM_UNIQUE_ID)
+                    and (command_type == MVU.COMMAND_TYPES.GET_MILAN_INFO)
                     then
                         -- Read protocol version (4 bytes)
                         local protocol_version = mvu_payload:int(4, 4)
@@ -146,8 +148,24 @@ function milan_proto.dissector(buffer, pinfo, tree)
 
                         -- Write feature flags to the MVU subtree
                         mvuSubtree:add(f_mvu_feature_flags, buffer(mvu_payload_start + 8, 4), feature_flags)
+                        -- Write individual features to the MVU subtree
                         mvuSubtree:add(f_mvu_feature_talker_dynamic_mappings_while_running, buffer(mvu_payload_start + 8, 4))
                         mvuSubtree:add(f_mvu_feature_redundancy, buffer(mvu_payload_start + 8, 4))
+                    end
+                    
+                    ---
+                    --- SET_SYSTEM_UNIQUE_ID commands or responses to GET_SYSTEM_UNIQUE_ID
+                    --- 
+                     
+                    -- if the mlessage is a SET_SYSTEM_UNIQUE_ID or a response to GET_SYSTEM_UNIQUE_ID
+                    if (message_type == IEEE17221.AECP_MESSAGE_TYPES.VENDOR_UNIQUE_COMMAND and command_type == MVU.COMMAND_TYPES.SET_SYSTEM_UNIQUE_ID)
+                    or (message_type == IEEE17221.AECP_MESSAGE_TYPES.VENDOR_UNIQUE_RESPONSE and command_type == MVU.COMMAND_TYPES.GET_SYSTEM_UNIQUE_ID)
+                    then
+                        -- Get system unique id
+                        local system_unique_id = mvu_payload:int(4, 4)
+
+                        -- Write system unique ID to the MVU subtree
+                        mvuSubtree:add(f_mvu_system_unique_id, buffer(mvu_payload_start + 4, 4), system_unique_id)
                     end
                 end
 
