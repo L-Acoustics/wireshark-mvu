559213296216130a7ca735fa9ed9eff3f9d8a7f8
benjamin.landrot <benjamin.landrot@l-acoustics.com>
2025-01-13 12:21:36 +0100

    Define MVU and IEEE1722.1 constants and rename some variables

--- mvu.lua
+++ mvu.lua
@@ -1,11 +1,41 @@
--- Constants
+---------------
+-- CONSTANTS --
+---------------
+---
+-- Version of this plugin
+VERSION = "1.0.0.0"
+
+-- MVU constants
+MVU = {}
+
+-- Version of the Milan Specification
+MVU.SPEC_VERSION = "1.2" -- Revision 1.2 of November 29, 2023
 
 --- Protocol ID for MVU
-local MVU_PROTOCOL_ID = "001bc50ac100"
+MVU.PROTOCOL_ID = "001bc50ac100"
 
---- List of known command types
-local COMMAND_TYPES = {
-    GET_MILAN_INFO = 0x0000, [0x0000] = "GET_MILAN_INFO"
+--- List of known MVU commands
+MVU.COMMAND_TYPES = {
+    GET_MILAN_INFO                 = 0x0000, [0x0000] = "GET_MILAN_INFO",
+    SET_SYSTEM_UNIQUE_ID           = 0x0001, [0x0001] = "SET_SYSTEM_UNIQUE_ID",
+    GET_SYSTEM_UNIQUE_ID           = 0x0002, [0x0002] = "GET_SYSTEM_UNIQUE_ID",
+    SET_MEDIA_CLOCK_REFERENCE_INFO = 0x0003, [0x0003] = "SET_MEDIA_CLOCK_REFERENCE_INFO",
+    GET_MEDIA_CLOCK_REFERENCE_INFO = 0x0004, [0x0004] = "GET_MEDIA_CLOCK_REFERENCE_INFO",
+}
+
+--- MVU status codes
+MVU.STATUS_CODES = {
+    SUCCESS         = 0, [0] = "SUCCESS",
+    NOT_IMPLEMENTED = 1, [1] = "NOT_IMPLEMENTED"
+}
+
+-- IEEE 1722.1 constants
+IEEE17221 = {}
+
+--- List of known IEEE 1722.1 AECP commands
+IEEE17221.AECP_MESSAGE_TYPES = {
+    VENDOR_UNIQUE_COMMAND  = 6, [6] = "VENDOR_UNIQUE_COMMAND",
+    VENDOR_UNIQUE_RESPONSE = 7, [7] = "VENDOR_UNIQUE_RESPONSE",
 }
 
 -- Create proto object for the dissector
@@ -17,15 +47,17 @@ local f_ieee17221_message_type = Field.new("ieee17221.message_type")
 local f_ieee17221_vendor_unique_protocol_id = Field.new("ieee17221.protocol_id")
 
 -- Declare new fields
-local f_milan_command_type = ProtoField.int32("mvu.command_type", "Command", base.DEC)
+local f_mvu_command_type = ProtoField.int32("mvu.command_type", "Command", base.DEC)
 
 -- Add fields to protocol
-milan_proto.fields = { f_milan_command_type }
+milan_proto.fields = {
+    f_mvu_command_type,
+}
 
 --- Implementation of protocol's dissector
---- @param buffer any
---- @param pinfo any
---- @param tree any
+--- @param buffer string The buffer to dissect
+--- @param pinfo table The packet info
+--- @param tree table The tree on which to add the procotol items
 function milan_proto.dissector(buffer, pinfo, tree)
 
     -- Read fields
@@ -50,7 +82,7 @@ function milan_proto.dissector(buffer, pinfo, tree)
             local vendor_unique_protocol_id = ieee17221_vendor_unique_protocol_id.range:bytes():tohex(true)
 
             -- Check field values
-            if vendor_unique_protocol_id == MVU_PROTOCOL_ID then
+            if vendor_unique_protocol_id == MVU.PROTOCOL_ID then
 
                 -- If packet visited
                 if pinfo.visited then
@@ -62,13 +94,17 @@ function milan_proto.dissector(buffer, pinfo, tree)
                     --   8 bytes for IEEE1722.1 controller ID
                     --   2 bytes for IEEE1722.1 sequence ID
                     --   6 bytes for IEEE1722.1 vendor protocol ID
-                    -- Note: the control_data_length includes IEEE1722.1 headers so the MVU payload size is the control data length - IEEE1722.1 headers length (16 bytes)
                     local mvu_payload_start = 14 + 4 + 8 + 8 + 2 + 6
+                    -- Note: the control_data_length includes IEEE1722.1 headers so the MVU payload size is the control data length - IEEE1722.1 headers length (16 bytes)
                     local mvu_payload_length = control_data_length - 16
                     local mvu_payload = buffer:bytes(mvu_payload_start, mvu_payload_length)
 
                     -- Add MVU subtree to packet details
-                    local milanSubtree = tree:add(milan_proto, buffer(42, control_data_length - 16), "Milan Vendor Unique (MVU)")
+                    local mvuSubtree = tree:add(milan_proto, buffer(42, control_data_length - 16), "Milan Vendor Unique (MVU)")
+
+                    ---
+                    --- Command Type
+                    ---
 
                     -- Read command type (2 bytes, ignoring first bit)
                     local command_type = 0x7fff & mvu_payload:int(0, 2)
@@ -76,7 +112,7 @@ function milan_proto.dissector(buffer, pinfo, tree)
                     local command_type_description = GetCommandTypeDescription(command_type)
 
                     -- Write command type and description to the MVU subtree
-                    milanSubtree:add(f_milan_command_type, buffer(42, 2), command_type):append_text(" (" .. command_type_description .. ")")
+                    mvuSubtree:add(f_mvu_command_type, buffer(mvu_payload_start, 2), command_type):append_text(" (" .. command_type_description .. ")")
                 end
 
             end
@@ -93,7 +129,7 @@ register_postdissector(milan_proto)
 function GetCommandTypeDescription(command_type)
 
     -- Get command type description from list
-    local description = COMMAND_TYPES[command_type]
+    local description = MVU.COMMAND_TYPES[command_type]
 
     -- If the description was found
     if type(description) == "string" then
