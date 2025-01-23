8f1f4cb0cf631df1c455e9f9fdfdfca1bbcf37d5
benjamin.landrot <benjamin.landrot@l-acoustics.com>
2025-01-23 17:52:07 +0100

    Treat NOT_IMPLEMENTED response to GET_MILAN_INFO like GET_MILAN_INFO command. Fix required CDL for GET_MILAN_INFO reponse

--- src/mvu_feature_milan_info.lua
+++ src/mvu_feature_milan_info.lua
@@ -85,13 +85,16 @@ function m.AddFieldsToSubtree(buffer, subtree)
 
 	-- Read IEEE 1722.1 field values
 	local message_type = mIEEE17221Fields.GetMessageType()
+	local status_code  = mIEEE17221Fields.GetVendorUniqueStatusCode()
 
 	-- Read MVU header field values
 	local command_type = mHeaders.GetCommandType()
 
-	-- If the message is a reponse to GET_MILAN_INFO
-	if 	message_type == mIEEE17221Specs.AECP_MESSAGE_TYPES.VENDOR_UNIQUE_RESPONSE
-	and command_type == mSpecs.COMMAND_TYPES.GET_MILAN_INFO
+	-- If the message is a GET_MILAN_INFO command
+	-- or is a NOT_IMPLEMENTED response to a GET_MILAN_INFO command
+	if 	command_type == mSpecs.COMMAND_TYPES.GET_MILAN_INFO
+	and ((message_type == mIEEE17221Specs.AECP_MESSAGE_TYPES.VENDOR_UNIQUE_COMMAND and status_code == mIEEE17221Specs.VENDOR_UNIQUE_STATUS_CODES.SUCCESS)
+	or (message_type == mIEEE17221Specs.AECP_MESSAGE_TYPES.VENDOR_UNIQUE_RESPONSE and status_code == mIEEE17221Specs.VENDOR_UNIQUE_STATUS_CODES.NOT_IMPLEMENTED))
 	then
 
 		-- Read Control data Length
@@ -124,14 +127,60 @@ function m.AddFieldsToSubtree(buffer, subtree)
 
 		end
 
+	end
+
+	-- If the message is a SUCCESS reponse to a GET_MILAN_INFO command
+	if 	message_type == mIEEE17221Specs.AECP_MESSAGE_TYPES.VENDOR_UNIQUE_RESPONSE
+	and status_code == mIEEE17221Specs.VENDOR_UNIQUE_STATUS_CODES.SUCCESS
+	and command_type == mSpecs.COMMAND_TYPES.GET_MILAN_INFO
+	then
+
+		-- Read Control data Length
+		local control_data_length = mIEEE17221Fields.GetControldataLength()
+
+		-- If the control data length is smaller than expected
+		-- Expected CDL for this command: 32
+		local expected_control_data_length = 32
+		if control_data_length < expected_control_data_length then
+
+			-- Build eror message
+			local error_message = "Control Data Length value is too small for this command"
+				.. " (CDL = " .. control_data_length
+				.. ", expected: " .. expected_control_data_length .. ")"
+
+			-- Get control data length error expert field from headers
+			local f_control_data_length_errors = mFields.GetExpertField("mvu.expert.control_data_length_error")
+
+			-- If expert field was found
+			if f_control_data_length_errors ~= nil then
+				-- Add control data length error to the subtree
+				subtree:add_tvb_expert_info(f_control_data_length_errors, buffer(16, 2), error_message)
+			end
+
+			-- Add error
+			table.insert(errors, error_message)
+
+			-- Do no more dissecting, stop function here
+			return errors
+
+		end
+
 		-- Get MVU payload bytes from buffer
 		local mvu_payload_bytes, mvu_payload_start, mvu_payload_length = mHeaders.GetMvuPayload()
+
+		--
+		-- Protocol version
+		--
 
 		-- Read protocol version (4 bytes)
 		local protocol_version = mvu_payload_bytes:int(4, 4)
 
 		-- Write protocol version to the MVU subtree
 		subtree:add(m._fields["mvu.protocol_version"], buffer(mvu_payload_start + 4, 4), protocol_version)
+
+		--
+		-- Feature flags
+		--
 
 		-- Read feature flags
 		local feature_flags = mvu_payload_bytes:int(8, 4)
