0ffe90a01c49d9de803dc8a695b9181b0ce458fc
benjamin.landrot <benjamin.landrot@l-acoustics.com>
2025-04-03 15:02:15 +0200

    Dump plugin version 1.3.0.0

--- src/mvu_plugin_info.lua
+++ src/mvu_plugin_info.lua
@@ -30,7 +30,7 @@ local m = {}
 ---------------------
 
 m._info = {
-	version = "1.2.1.0",
+	version = "1.3.0.0",
 	author = "L-Acoustics",
 	description = "Lua plugin for dissecting Milan Vendor Unique information in IEEE1722.1 frames in Wireshark",
 	repository = "https://github.com/L-Acoustics/wireshark-mvu"
