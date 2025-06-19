93dbca02171fc99d9ff0351fbdabb74668d681e9
benjamin.landrot <benjamin.landrot@l-acoustics.com>
2025-06-19 11:34:39 +0200

    Update plugin maximum supported Milan version

--- src/mvu_specs.lua
+++ src/mvu_specs.lua
@@ -34,7 +34,7 @@ local m = {}
 -----------------------
 
 -- Version of the Milan Specification
-m.SPEC_VERSION = "1.2.10" -- Revision 1.2.10 of TBC, 2025
+m.SPEC_VERSION = "1.2.12" -- Revision 1.2.12 of TBC, 2025
 
 -- Protocol ID for MVU
 m.PROTOCOL_ID = "0x001BC50AC100"
